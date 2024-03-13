275f0a627bf58c6321050c12988baaf26bd692d2
jmpure <jmpure2@naver.com>
2024-03-13 13:24:58 +0900

    update source

--- src/Client.java
+++ src/Client.java
@@ -7,53 +7,22 @@ import java.util.StringTokenizer;
 public class Client {
     public static void main(String[] args) throws IOException {
         SortPrinterMachine sortMachine = new SortPrinterMachine();
-        int sortType;
-        ArrayList<Integer> numList = new ArrayList<>();
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("입력할 숫자의 개수를 말해주세요 : ");
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int numCount = Integer.parseInt(st.nextToken());
-
-        System.out.println("정렬할 숫자들을 입력해주세요 : (ex. 1 2 3 4 5 6)");
-        st = new StringTokenizer(br.readLine());
 
         try {
-            for (int i = 0; i < numCount; i++) {
-                numList.add(Integer.parseInt(st.nextToken()));
-            }
+            // 숫자 List 입력
+            sortMachine.insert();
+
+            // Sort Type 선택
+            sortMachine.selection();
+
+            // Sort 알고리즘 수행
+            sortMachine.run();
+
+            // 정렬된 결과 출력
+            sortMachine.show();
         }
         catch (Exception e) {
-            System.out.println("Abnormal : 입력한 숫자의 개수가 부족합니다!");
-            return;
+            System.out.println(">>> 다시 기계를 동작시켜 주세요!!");
         }
-
-        // 숫자 List 입력
-        sortMachine.insert(numList);
-
-        System.out.println("정렬 방법을 선택해주세요 : (1:Bubble Sort, 2:Selection Sort)");
-        st = new StringTokenizer(br.readLine());
-
-        sortType = Integer.parseInt(st.nextToken());
-
-        // Sort Type 선택
-        switch (sortType) {
-            case 1:
-                sortMachine.selection(new BubbleSort());
-                break;
-            case 2:
-                sortMachine.selection(new SelectionSort());
-                break;
-            default:
-                System.out.println("현재 지원하지 않는 정렬 방법입니다.");
-                return;
-        }
-
-        // Sort 알고리즘 수행
-        sortMachine.run();
-        // 정렬된 결과 출력
-        sortMachine.show();
     }
 }
--- src/SortPrinterMachine.java
+++ src/SortPrinterMachine.java
@@ -1,15 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class SortPrinterMachine {
-    private ArrayList<Integer> numList;
+    private ArrayList<Integer> numList = new ArrayList<>();
     private SortModule printer;
+    private int sortType;
+    BufferedReader br;
+    StringTokenizer st;
 
-    public void insert(ArrayList<Integer> numList) {
-        this.numList = numList;
+    public void insert() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("입력할 숫자의 개수를 말해주세요 : ");
+        st = new StringTokenizer(br.readLine());
+
+        int numCount = Integer.parseInt(st.nextToken());
+
+        System.out.println("정렬할 숫자들을 입력해주세요 : (ex. 1 2 3 4 5 6)");
+        st = new StringTokenizer(br.readLine());
+
+        try {
+            for (int i = 0; i < numCount; i++) {
+                numList.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        catch (Exception e) {
+            System.out.println("ERROR : 입력한 숫자의 개수가 부족합니다!");
+            throw new IOException();
+        }
     }
 
-    public void selection(SortModule printer) {
-        this.printer = printer;
+    public void selection() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("정렬 방법을 선택해주세요 : (1:Bubble Sort, 2:Selection Sort)");
+        st = new StringTokenizer(br.readLine());
+
+        sortType = Integer.parseInt(st.nextToken());
+
+        switch (sortType) {
+            case 1:
+                printer = new BubbleSort();
+                break;
+            case 2:
+                printer = new SelectionSort();
+                break;
+            default:
+                System.out.println("ERROR : 현재 지원하지 않는 정렬 방법입니다.");
+                throw new IOException();
+        }
     }
 
     public void run() {
